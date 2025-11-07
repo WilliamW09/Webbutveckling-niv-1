fec5bf4efff7b732c5737a0531dc96adc0bf1196
WilliamW09 <william.widenfalk@gmail.com>
2025-11-07 10:23:15 +0100

    [ADD] Hamburger to Nav

--- Bobergsgymnasiet/index.js
+++ Bobergsgymnasiet/index.js
@@ -1,6 +1,19 @@
+const mobileNav = document.querySelector(".mobile-nav");
+const hamburgerBtn = document.querySelector("#menu-open")
+
 const closeMenu = () => {
-    const nav = document. querySelector("nav");
-    nav.classList.add("hidden")
+    mobileNav.classList.add("hidden")
+       hamburgerBtn.classList.remove("hidden")
+}
+const openMenu = () => {
+    hamburgerBtn.classList.add("hidden")
+    mobileNav.classList.remove("hidden")
 }
 
-const menuCloseBtn = document.querySelector(".menu-close-btn").addEventListener("click", closeMenu)
+const menuCloseBtn = document
+.querySelector("#menu-close")
+.addEventListener("click", closeMenu);
+
+const menuOpenBtn = document
+.querySelector("#menu-open")
+.addEventListener("click", openMenu);
