d313f43a5db260e5036ea4575a02c5f0b72e15b0
WilliamW09 <william.widenfalk@gmail.com>
2025-11-14 10:24:02 +0100

    [ADD] digital something

--- Bobergsgymnasiet/index.js
+++ Bobergsgymnasiet/index.js
@@ -2,6 +2,28 @@ const nav = document.querySelector("nav");
 const mobileNav = document.querySelector(".mobile-nav");
 const hamburgerBtn = document.querySelector("#menu-open");
 
+const news = [
+    {
+        id: "1",
+        img: "./img /"
+        title: "Föredrag 22/10",
+        date: "16 Oktober 2025"
+    },
+    {
+        id: "2",
+        img: "",
+        title: "Åk 9 besöksdag",
+        date: "5 Oktober 2025"
+    },
+    {
+        id: "3"
+        img: "",
+        title: "föreldramöte åk 1 9/9",
+        date: "2 september 2025"
+    },
+]
+
+
 const closeMenu = () => {
     mobileNav.classList.add("hidden");
     nav.classList.add("nav-closed");
@@ -20,3 +42,22 @@ const menuCloseBtn = document
 const menuOpenBtn = document
 .querySelector("#menu-open")
 .addEventListener("click", openMenu);
+
+const renderNews = () => {
+    const container = document.querySelector(".news-grid");
+
+    news.forEach = ((news) => {
+        const card = document.createElement("div");
+        card.className = "news-card";
+
+        card.innerHTML = `
+            <img src="${news.img} alt="${new.title}" class="news-image" />
+            <h3 class="news-title">${news.title}</h3>
+            <p class="news-date">${news.date}</p>
+            `/
+
+            container.appendChild(card);
+    });
+};
+
+renderNews();
